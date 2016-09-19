702a032df67fb98b57020b958750ff3b8eab9824
ajivallakkatu <ajivallakkatu@gmail.com>
2016-09-19 23:17:22 +0530

    Made some changes

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/ksdry.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'ksdry.png'));
+app.get('/ui/ajiA.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'ajiA.jpg'));
 });
 
 
